30ddf496ed58d296d74c305d8e206302024e9e9f
mahithsc <mahith.chitrapu@gmail.com>
2023-03-01 10:47:43 -0800

    commenting code to make it more readable

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import cv2
 from FaceRecognition import FaceRecognition
 
 if __name__ == '__main__':
+
+    # getting the webcam
     cap = cv2.VideoCapture(0)
+
+    # initializing the facial recognition module
     detector = FaceRecognition()
 
     while True:
+        # getting image from webcam
         success, img = cap.read()
+        # getting deteted faces
         recs = detector.get_recognitions(img = img)
+        # printing image to screen
         cv2.imshow("Image", detector.draw_landmarks(img=img))
         cv2.waitKey(1)
