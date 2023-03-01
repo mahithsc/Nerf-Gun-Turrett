9fb7aee6ff48c51e7fdd882b4cd02e01a237ebf3
mahithsc <mahith.chitrapu@gmail.com>
2023-02-28 21:13:13 -0800

    capturing video from web cam and getting frame rate

--- face.py
+++ face.py
@@ -7,3 +7,14 @@ import time
 cap = cv2.VideoCapture(0)
 pTime = 0
 
+while True:
+    success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime 
+    cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
