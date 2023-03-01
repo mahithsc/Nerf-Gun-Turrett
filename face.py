e400b6580c5ffdd030a8c1bda97c9f3632fb9a89
mahithsc <mahith.chitrapu@gmail.com>
2023-02-28 21:53:01 -0800

    putting the needed data in the screen

--- face.py
+++ face.py
@@ -14,19 +14,17 @@ faceDetection = mpFaceDetection.FaceDetection()
 while True:
     success, img = cap.read()
 
-
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = faceDetection.process(imgRGB)
 
     if results.detections:
         for id, detection in enumerate(results.detections):
-            # print(detection.location_data.relative_bounding_box)
+            print(detection)
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-            print(bbox[0])
             cv2.rectangle(img, bbox, (255, 0, 255), 3)
-            cv2.putText(img, f'Index: 1 | Confidence: 100%', (bbox[0], bbox[1]), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
+            cv2.putText(img, f'Index:  {id + 1} | Confidence: {int(detection.score[0] * 100)}%', (bbox[0], bbox[1]), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
