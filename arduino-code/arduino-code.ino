a036971b7751989a15115f73a8c56d2908f9e2cd
mahithsc <mahith.chitrapu@gmail.com>
2023-03-01 16:16:57 -0800

    making sure the servo works with the serial data

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,22 +1,30 @@
+#include <Servo.h>
+
 int incomingByte = 0;
+Servo myservo;
 
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
+  myservo.attach(9);
+  myservo.write(0);
 }
 
 void loop() {
+  delay(40);
   digitalWrite(13, LOW);
   if(Serial.available() > 0) {
     if(Serial.read() != NULL) {
       digitalWrite(13, HIGH);
-      delay(30);
+      myservo.write(90);
     }
     else{
       digitalWrite(13, LOW);
+      myservo.write(0);
     }
   } else {
     digitalWrite(13, LOW);
+    myservo.write(0);
   }
 }
